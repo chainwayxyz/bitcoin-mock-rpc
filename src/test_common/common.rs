936fa270eed71036c557df705309d5d75f292bd5
Ceyhun Şen <ceyhuusen@gmail.com>
2024-06-06 15:33:38 +0300

    tests: Delete unnecessary test files.

--- src/test_common/common.rs
+++
@@ -1,5 +0,0 @@
-//! # Common
-//!
-//! Common crate includes stuff that every crate can use, AKA common stuff.
-
-pub fn get_temp_public_keys() {}
--- tests/example.rs
+++
@@ -1,9 +0,0 @@
-//! Example integration test.
-
-mod common;
-
-#[test]
-fn example_test() {
-	let _ = common::get_temp_public_keys();
-	let _ = common::Config::new();
-}
