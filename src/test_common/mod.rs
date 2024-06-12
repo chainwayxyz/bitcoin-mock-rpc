99cea8192c90ff268bd8f128242fefa9e5efe148
Ceyhun Şen <ceyhuusen@gmail.com>
2024-06-12 17:47:22 +0300

    test_common: Add create_txin.

--- src/test_common/mod.rs
+++ src/test_common/mod.rs
@@ -5,8 +5,16 @@
 //! This crate is in `src/` directory because unit tests can't access `tests/`
 //! directory.
 
-use bitcoin::{absolute, Address, Network, Transaction, TxIn, TxOut, XOnlyPublicKey};
+use bitcoin::{
+    absolute,
+    key::UntweakedPublicKey,
+    opcodes::all::OP_EQUAL,
+    taproot::{LeafVersion, TaprootBuilder},
+    Address, Network, OutPoint, ScriptBuf, Transaction, TxIn, TxOut, Txid, Witness, WitnessProgram,
+    XOnlyPublicKey,
+};
 use secp256k1::Secp256k1;
+use std::str::FromStr;
 
 #[allow(unused)]
 pub fn get_temp_address() -> Address {
@@ -19,6 +27,47 @@ pub fn get_temp_address() -> Address {
     .unwrap();
 
     Address::p2tr(&secp, xonly_public_key, None, Network::Regtest)
+}
+
+#[allow(unused)]
+pub fn create_txin(txid: Txid) -> TxIn {
+    let secp = bitcoin::secp256k1::Secp256k1::new();
+    let internal_key = UntweakedPublicKey::from(
+        bitcoin::secp256k1::PublicKey::from_str(
+            "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0",
+        )
+        .unwrap(),
+    );
+
+    let mut script = ScriptBuf::new();
+    script.push_slice([12, 34]);
+    script.push_instruction(bitcoin::script::Instruction::Op(OP_EQUAL));
+
+    let taproot_builder = TaprootBuilder::new().add_leaf(0, script.clone()).unwrap();
+    let taproot_spend_info = taproot_builder.finalize(&secp, internal_key).unwrap();
+
+    let witness_program =
+        WitnessProgram::p2tr(&secp, internal_key, taproot_spend_info.merkle_root());
+
+    let mut control_block_bytes = Vec::new();
+    taproot_spend_info
+        .control_block(&(script.clone(), LeafVersion::TapScript))
+        .unwrap()
+        .encode(&mut control_block_bytes)
+        .unwrap();
+
+    let mut script2 = ScriptBuf::new();
+    script2.push_slice([12, 34]);
+    let mut witness = Witness::new();
+    witness.push(script2);
+    witness.push(script.to_bytes());
+    witness.push(control_block_bytes);
+
+    TxIn {
+        previous_output: OutPoint { txid, vout: 0 },
+        witness,
+        ..Default::default()
+    }
 }
 
 #[allow(unused)]
