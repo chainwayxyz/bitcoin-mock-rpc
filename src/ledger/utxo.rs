62d85ebc1be5850b99eea0741ac9323d8a7b5efd
Ceyhun Şen <ceyhuusen@gmail.com>
2024-06-25 18:22:50 +0300

    utxo: Add combine_utxos.

--- src/ledger/utxo.rs
+++ src/ledger/utxo.rs
@@ -20,6 +20,39 @@ impl Ledger {
         return_vec_item!(self.utxos);
     }
 
+    /// Combines UTXO's which equals or more of the specified amount. This is
+    /// useful for generating TxIn's.
+    ///
+    /// UTXO's are selected with FIFO technique. TODO: This can be optimized later.
+    ///
+    /// # Returns
+    ///
+    /// Returns UTXO's in a `Vec` and their total value.
+    pub fn combine_utxos(&self, amount: Amount) -> Result<(Vec<OutPoint>, Amount), LedgerError> {
+        let mut total_value = Amount::from_sat(0);
+        let mut utxos = Vec::new();
+
+        for utxo in self.get_utxos() {
+            let tx = self.get_transaction(utxo.txid)?;
+            let txout = tx.output.get(utxo.vout as usize).unwrap();
+
+            total_value += txout.value;
+            utxos.push(utxo);
+
+            if total_value >= amount {
+                break;
+            }
+        }
+
+        if amount > total_value {
+            return Err(LedgerError::UTXO(format!(
+                "Requested amount bigger than balance: {amount} > {total_value}"
+            )));
+        }
+
+        Ok((utxos, total_value))
+    }
+
     /// Calculate balance using UTXO's.
     pub fn calculate_balance(&self) -> Result<Amount, LedgerError> {
         let mut amount = Amount::from_sat(0);
@@ -108,22 +141,22 @@ mod tests {
         assert_eq!(ledger.calculate_balance().unwrap(), Amount::from_sat(0));
 
         // Insert dummy transactions.
-        let txout = ledger.create_txout(100 - 0x1F, None);
+        let txout = ledger.create_txout(Amount::from_sat(100 - 0x1F), None);
         let tx = ledger.create_transaction(vec![], vec![txout]);
         let txid = tx.compute_txid();
         let utxo = OutPoint { txid, vout: 0 };
         add_item_to_vec!(ledger.utxos, utxo);
         add_item_to_vec!(ledger.transactions, tx);
 
-        let txout = ledger.create_txout(0x1F, None);
+        let txout = ledger.create_txout(Amount::from_sat(0x1F), None);
         let tx = ledger.create_transaction(vec![], vec![txout]);
         let txid = tx.compute_txid();
         let utxo = OutPoint { txid, vout: 0 };
         add_item_to_vec!(ledger.utxos, utxo);
         add_item_to_vec!(ledger.transactions, tx);
 
-        let txout1 = ledger.create_txout(100 - 0x1F, None);
-        let txout2 = ledger.create_txout(0x1F, None);
+        let txout1 = ledger.create_txout(Amount::from_sat(100 - 0x1F), None);
+        let txout2 = ledger.create_txout(Amount::from_sat(0x1F), None);
         let tx = ledger.create_transaction(vec![], vec![txout1, txout2]);
         let txid = tx.compute_txid();
         let utxo1 = OutPoint { txid, vout: 0 };
@@ -143,5 +176,43 @@ mod tests {
             ledger.calculate_balance().unwrap(),
             Amount::from_sat(200 - 0x1F)
         );
+    }
+
+    #[test]
+    fn combine_utxos() {
+        let ledger = Ledger::new();
+        let address = ledger.generate_credential().address;
+
+        // Add some small UTXO's to user.
+        for i in 0..100 {
+            let txout = ledger.create_txout(Amount::from_sat(i), Some(address.script_pubkey()));
+            let tx = ledger.create_transaction(vec![], vec![txout]);
+            ledger.add_transaction_unconditionally(tx).unwrap();
+        }
+
+        // Because combining currently uses FIFO algorithm for choosing UTXO's
+        // and we know what are getting pushed, we can guess correct txin value.
+        assert_eq!(
+            ledger.combine_utxos(Amount::from_sat(1)).unwrap().1,
+            Amount::from_sat(1)
+        );
+        assert_eq!(
+            ledger.combine_utxos(Amount::from_sat(4)).unwrap().1,
+            Amount::from_sat(6)
+        );
+        assert_eq!(
+            ledger.combine_utxos(Amount::from_sat(10)).unwrap().1,
+            Amount::from_sat(10)
+        );
+        assert_eq!(
+            ledger.combine_utxos(Amount::from_sat(11)).unwrap().1,
+            Amount::from_sat(15)
+        );
+
+        // Trying to request an amount bigger than current balance should throw
+        // an error.
+        if let Ok(_) = ledger.combine_utxos(Amount::from_sat((0..100).sum::<u64>() + 1)) {
+            assert!(false);
+        }
     }
 }
