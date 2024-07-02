b85e3e57306c424b940cfd3dacac227c278ce132
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-02 11:51:19 +0300

    integration_test: Add send_get_raw_transaction_async test and add Tokio as a dev dependency.

--- tests/transaction.rs
+++ tests/transaction.rs
@@ -1,12 +1,17 @@
 //! Transaction related integration tests.
 
-use bitcoin::{Amount, OutPoint, TxIn};
+use bitcoin::{Amount, OutPoint, Transaction, TxIn, TxOut};
 use bitcoin_mock_rpc::{Client, RpcApiWrapper};
 use bitcoincore_rpc::{Auth, RpcApi};
 use std::thread;
+use tokio::join;
 
 mod common;
 use common::test_common;
+
+async fn send_raw_transaction_async(rpc: Client, tx: Transaction) {
+    rpc.send_raw_transaction(&tx).unwrap();
+}
 
 #[test]
 #[ignore = "Not necessary after the send_to_address simplification"]
@@ -112,4 +117,72 @@ fn use_utxo_from_send_to_address() {
     if let Ok(_) = rpc.send_raw_transaction(&tx) {
         assert!(false);
     };
+}
+
+#[tokio::test]
+async fn send_get_raw_transaction_async() {
+    let rpc = Client::new("", Auth::None).unwrap();
+
+    let address = rpc.get_new_address(None, None).unwrap().assume_checked();
+
+    let txid1 = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45 * 0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+    let txid2 = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45 * 0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+
+    let txin = TxIn {
+        previous_output: OutPoint {
+            txid: txid1,
+            vout: 0,
+        },
+        ..Default::default()
+    };
+    let txout = TxOut {
+        value: Amount::from_sat(0x45),
+        script_pubkey: address.script_pubkey(),
+    };
+    let tx1 = test_common::create_transaction(vec![txin], vec![txout]);
+
+    let txin = TxIn {
+        previous_output: OutPoint {
+            txid: txid2,
+            vout: 0,
+        },
+        ..Default::default()
+    };
+    let txout = TxOut {
+        value: Amount::from_sat(0x1F),
+        script_pubkey: address.script_pubkey(),
+    };
+    let tx2 = test_common::create_transaction(vec![txin], vec![txout]);
+
+    let async_thr1 = send_raw_transaction_async(rpc.clone(), tx1);
+    let async_thr2 = send_raw_transaction_async(rpc.clone(), tx2);
+
+    join!(async_thr1, async_thr2);
+
+    assert_eq!(
+        rpc.get_balance(None, None).unwrap(),
+        Amount::from_sat(0x45 + 0x1F)
+    );
 }
