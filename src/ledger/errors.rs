6bbb3fb0e31253947d34abdc34ed5f797dd2951a
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-03 18:03:22 +0300

    errors: Delete general error type.

--- src/ledger/errors.rs
+++ src/ledger/errors.rs
@@ -7,8 +7,6 @@ use thiserror::Error;
 /// Ledger error types.
 #[derive(Error, Debug)]
 pub enum LedgerError {
-    #[error("General error: {0}")]
-    General(String),
     #[error("Transaction error: {0}")]
     Transaction(String),
     #[error("UTXO error: {0}")]
