98f0e8396ec489791412b3ed72651ee2b02bc0c3
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-04 12:35:29 +0300

    tests: raw_transactions: Add send_get_raw_transaction_with_change.

--- tests/raw_transactions.rs
+++ tests/raw_transactions.rs
@@ -9,6 +9,45 @@ use tokio::join;
 mod common;
 
 #[test]
+fn send_get_raw_transaction_with_change() {
+    let rpc = Client::new("", Auth::None).unwrap();
+
+    let address = rpc.get_new_address(None, None).unwrap().assume_checked();
+    let deposit_address = common::create_address_from_witness();
+
+    // Generate funds to user.
+    let txid = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45 * 0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+    assert_eq!(
+        rpc.get_balance(None, None).unwrap(),
+        Amount::from_sat(0x45 * 0x45)
+    );
+
+    let txin = common::create_txin(txid, 0);
+    let txout0 = common::create_txout(Amount::from_sat(0x45), deposit_address.script_pubkey());
+    let txout1 = common::create_txout(Amount::from_sat(0x45 * 0x44), address.script_pubkey());
+    let tx = common::create_transaction(vec![txin], vec![txout0, txout1]);
+    let txid = rpc.send_raw_transaction(&tx).unwrap();
+
+    assert_eq!(
+        rpc.get_balance(None, None).unwrap(),
+        Amount::from_sat(0x45 * 0x44)
+    );
+
+    assert_eq!(rpc.get_raw_transaction(&txid, None).unwrap(), tx);
+}
+
+#[test]
 fn send_get_raw_transaction_without_change() {
     let rpc = Client::new("", Auth::None).unwrap();
 
