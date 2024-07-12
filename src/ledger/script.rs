93cb8aece26ab617c31aaa71dd9948f561ddad83
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-12 11:31:26 +0300

    ledger: script: Add CSV check.

--- src/ledger/script.rs
+++ src/ledger/script.rs
@@ -1,27 +1,55 @@
 //! # Script Related Ledger Operations
 
 use super::{errors::LedgerError, Ledger};
-use bitcoin::ScriptBuf;
+use bitcoin::{opcodes::all::{OP_CSV, OP_PUSHNUM_1}, script, ScriptBuf};
 use bitcoin_scriptexec::{Exec, ExecCtx, Options, TxTemplate};
 
 impl Ledger {
     pub fn run_script(
+        &self,
         ctx: ExecCtx,
         tx_template: TxTemplate,
         script_buf: ScriptBuf,
         script_witness: Vec<Vec<u8>>,
     ) -> Result<(), LedgerError> {
+        let _prev_outs = tx_template.prevouts.clone();
+        let _input_idx = tx_template.input_idx.clone();
+
         let mut exec = Exec::new(
             ctx,
             Options::default(),
             tx_template,
-            script_buf,
+            script_buf.clone(),
             script_witness,
         )
         .map_err(|e| LedgerError::SpendingRequirements(format!("Script format error: {:?}", e)))?;
 
+        // Check for CSV
+        if {
+            let mut instructions = script_buf.instructions();
+            let op1 = instructions.next();
+            let op2 = instructions.next();
+
+            if let (Some(Ok(op1)), Some(Ok(op2))) = (op1, op2) {
+                if op2 == script::Instruction::Op(OP_CSV) {
+                    let height = op1.opcode().unwrap().to_u8();
+                    let height = height - (OP_PUSHNUM_1.to_u8() - 1);
+
+                    let current_height = self.get_block_height().unwrap();
+
+                    println!("-- {:?}, {:?}, {:?}, {:?}, ", op1, op2, height, current_height);
+                }
+            }
+
+            true
+        } == false
+        {
+            return Err(LedgerError::Script(format!(
+                "TX is locked for the block height: "
+            )));
+        }
+
         loop {
-            println!("res adim {:?}", exec.remaining_script());
             let res = exec.exec_next();
             if res.is_err() {
                 break;
