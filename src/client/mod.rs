cf0e77afa89f94407eb09587747188f485804230
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-25 11:32:22 +0300

    general: Add a custom clone support.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -13,12 +13,21 @@ mod rpc_api;
 /// this via trait definitions. This is helpful for choosing different rpc
 /// interface between test and release builds.
 pub trait RpcApiWrapper: RpcApi + std::marker::Sync + std::marker::Send + 'static {
+    /// Create a new RPC connection.
     fn new(url: &str, auth: Auth) -> bitcoincore_rpc::Result<Self>;
+    /// Create a new RPC connection, without any cleanup. Useful for `clone`
+    /// calls.
+    fn new_without_cleanup(url: &str, auth: Auth) -> bitcoincore_rpc::Result<Self>;
 }
 
 /// Compatibility implementation for `bitcoincore_rpc::Client`.
 impl RpcApiWrapper for bitcoincore_rpc::Client {
     fn new(url: &str, auth: Auth) -> bitcoincore_rpc::Result<Self> {
+        bitcoincore_rpc::Client::new(url, auth)
+    }
+
+    /// Same as `new`.
+    fn new_without_cleanup(url: &str, auth: Auth) -> bitcoincore_rpc::Result<Self> {
         bitcoincore_rpc::Client::new(url, auth)
     }
 }
@@ -41,6 +50,15 @@ impl RpcApiWrapper for Client {
     fn new(url: &str, _auth: bitcoincore_rpc::Auth) -> bitcoincore_rpc::Result<Self> {
         Ok(Self {
             ledger: Ledger::new(url),
+        })
+    }
+
+    /// This function will establish a new database connection, while preserving
+    /// it's previous state. Meaning it won't clear database. This is helpful
+    /// for cloning the `Client` structure.
+    fn new_without_cleanup(url: &str, _auth: Auth) -> bitcoincore_rpc::Result<Self> {
+        Ok(Self {
+            ledger: Ledger::new_without_cleanup(url),
         })
     }
 }
