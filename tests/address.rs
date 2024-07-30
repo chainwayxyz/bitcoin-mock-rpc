733f1ae049837ee7c273c424e1f4244fbfc0e3c7
Ceyhun Şen <ceyhuusen@gmail.com>
2024-07-30 12:53:40 +0300

    tests: address: Check block height and time.

--- tests/address.rs
+++ tests/address.rs
@@ -1,5 +1,6 @@
 //! Address related integration tests.
 
+use bitcoin::Amount;
 use bitcoin_mock_rpc::{Client, RpcApiWrapper};
 use bitcoincore_rpc::{Auth, RpcApi};
 use std::thread;
@@ -10,7 +11,27 @@ fn generate_to_address() {
     let address = rpc.get_new_address(None, None).unwrap().assume_checked();
 
     // Should increase block height.
+    rpc.generate_to_address(1, &address).unwrap();
+
+    let txid = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+
     rpc.generate_to_address(101, &address).unwrap();
+
+    let details = rpc.get_transaction(&txid, None).unwrap();
+
+    assert_eq!(details.info.blockheight, Some(102));
+    assert_eq!(details.info.confirmations, 101);
 }
 
 #[test]
@@ -24,10 +45,50 @@ fn generate_to_address_multi_threaded() {
         cloned_rpc
             .generate_to_address(101, &cloned_address)
             .unwrap();
+
+        let txid = cloned_rpc
+            .send_to_address(
+                &cloned_address,
+                Amount::from_sat(0x45),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            )
+            .unwrap();
+
+        cloned_rpc
+            .generate_to_address(101, &cloned_address)
+            .unwrap();
+
+        let details = cloned_rpc.get_transaction(&txid, None).unwrap();
+
+        assert_eq!(details.info.blockheight, Some(202));
+        assert_eq!(details.info.confirmations, 101);
     })
     .join()
     .unwrap();
 
     // Adding new blocks should increase block height.
     rpc.generate_to_address(101, &address).unwrap();
+
+    let txid = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+
+    let details = rpc.get_transaction(&txid, None).unwrap();
+
+    assert_eq!(details.info.blockheight, Some(303));
+    assert_eq!(details.info.confirmations, 0);
 }
