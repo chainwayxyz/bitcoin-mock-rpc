2f87802178a1d13d89a63570d3183e8418efeada
Ceyhun Şen <ceyhuusen@gmail.com>
2024-08-09 17:41:01 +0300

    tests: Add auth to Python helper for regtest.

--- tests/json_rpc_call.py
+++ tests/json_rpc_call.py
@@ -2,11 +2,16 @@
 #
 # JSON RPC call example.
 
+from requests.auth import HTTPBasicAuth
 import requests
 
 url = "http://127.0.0.1:1024"
+# url = "http://127.0.0.1:18443"
+
+auth = HTTPBasicAuth("admin", "admin")
+
 method = "getrawtransaction"
-params = {"txid": "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87"} 
+params = {"txid": "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87"}
 
 payload = {
     "jsonrpc": "2.0",
@@ -15,5 +20,6 @@ payload = {
     "id": 1,
 }
 
-response = requests.post(url, json=payload)
+response = requests.post(url, json=payload, auth=auth)
+
 print(response.json())
