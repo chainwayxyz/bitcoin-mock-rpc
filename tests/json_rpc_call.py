9eee5cadf364334272716c3f9268cab300a00b38
Ceyhun Şen <ceyhuusen@gmail.com>
2024-08-09 15:40:28 +0300

    tests: Update Python helper method.

--- tests/json_rpc_call.py
+++ tests/json_rpc_call.py
@@ -5,8 +5,8 @@
 import requests
 
 url = "http://127.0.0.1:1024"
-method = "sendrawtransaction"
-params = {"tx": "dummy"} 
+method = "getrawtransaction"
+params = {"txid": "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87"} 
 
 payload = {
     "jsonrpc": "2.0",
