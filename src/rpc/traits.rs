67d71d6b61cb35c51ef3b92339267f3988aaeb7f
Ceyhun Şen <ceyhuusen@gmail.com>
2024-08-09 15:39:51 +0300

    rpc: Don't use strings; Use actual types.

--- src/rpc/traits.rs
+++ src/rpc/traits.rs
@@ -8,7 +8,6 @@ use bitcoincore_rpc::RpcApi;
 use jsonrpsee::core::async_trait;
 use jsonrpsee::proc_macros::rpc;
 use jsonrpsee::types::ErrorObjectOwned;
-use std::str::FromStr;
 
 /// Holds ledger connection.
 pub struct InnerRpc {
@@ -23,8 +22,8 @@ pub trait Rpc {
     #[method(name = "getrawtransaction")]
     async fn getrawtransaction(
         &self,
-        txid: String,
-        block_hash: Option<String>,
+        txid: Txid,
+        block_hash: Option<BlockHash>,
     ) -> Result<String, ErrorObjectOwned>;
 }
 
@@ -42,31 +41,10 @@ impl RpcServer for InnerRpc {
 
     async fn getrawtransaction(
         &self,
-        txid: String,
-        block_hash: Option<String>,
+        txid: Txid,
+        block_hash: Option<BlockHash>,
     ) -> Result<String, ErrorObjectOwned> {
-        let txid = match Txid::from_str(&txid) {
-            Ok(txid) => txid,
-            Err(_) => {
-                return Err(ErrorObjectOwned::from(
-                    jsonrpsee::types::ErrorCode::InvalidParams,
-                ))
-            }
-        };
-        let bh = match block_hash {
-            Some(bh) => match BlockHash::from_str(&bh) {
-                Ok(bh) => Some(bh),
-                Err(_) => {
-                    return Err(ErrorObjectOwned::from(
-                        jsonrpsee::types::ErrorCode::InvalidParams,
-                    ))
-                }
-            },
-            _ => None,
-        };
-        let block_hash = bh.as_ref();
-
-        if let Ok(res) = self.client.get_raw_transaction(&txid, block_hash) {
+        if let Ok(res) = self.client.get_raw_transaction(&txid, block_hash.as_ref()) {
             let mut hex: Vec<u8> = Vec::new();
             if let Err(_) = res.consensus_encode(&mut hex) {
                 return Err(ErrorObjectOwned::from(
