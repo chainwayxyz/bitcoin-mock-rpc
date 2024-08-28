d66e05e51a5a8c76cc0400e359223827aeed65bc
Ceyhun Şen <ceyhuusen@gmail.com>
2024-08-28 11:30:30 +0300

    tests: Add transaction_related_rpc_calls rpc test.

--- tests/rpc.rs
+++ tests/rpc.rs
@@ -5,10 +5,15 @@
 //! correctness of the call results aren't necessarily important for these test.
 //! It is the job of other tests.
 
+use bitcoin::absolute::Height;
+use bitcoin::transaction::Version;
+use bitcoin::{Amount, OutPoint, Transaction, TxIn, TxOut};
 use bitcoin_mock_rpc::rpc::spawn_rpc_server;
 use bitcoincore_rpc::RpcApi;
 use jsonrpsee::core::client::ClientT;
 use jsonrpsee::{http_client::HttpClient, rpc_params};
+
+mod common;
 
 #[tokio::test]
 async fn check_server_availability() {
@@ -34,7 +39,7 @@ fn create_connection() {
 }
 
 #[test]
-fn address_related() {
+fn address_related_rpc_calls() {
     let server = spawn_rpc_server(None, None).unwrap();
     let url = server.0.to_string();
     println!("Server started at {url}");
@@ -46,7 +51,7 @@ fn address_related() {
 }
 
 #[test]
-fn block_related() {
+fn block_related_rpc_calls() {
     let server = spawn_rpc_server(None, None).unwrap();
     let url = server.0.to_string();
     println!("Server started at {url}");
@@ -72,4 +77,50 @@ fn block_related() {
         .unwrap()
         .unwrap();
     assert_eq!(txout.confirmations, 1);
+}
+
+#[test]
+fn transaction_related_rpc_calls() {
+    let server = spawn_rpc_server(None, None).unwrap();
+    let url = server.0.to_string();
+    println!("Server started at {url}");
+
+    let rpc = bitcoincore_rpc::Client::new(url.as_str(), bitcoincore_rpc::Auth::None).unwrap();
+
+    let witness = common::create_witness();
+    let address = common::create_address_from_witness(witness.0);
+
+    let txid = rpc
+        .send_to_address(
+            &address,
+            Amount::from_sat(0x45 * 0x45),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+
+    let _tx = rpc.get_raw_transaction(&txid, None).unwrap();
+
+    let txin = TxIn {
+        previous_output: OutPoint { txid, vout: 0 },
+        witness: witness.1,
+        ..Default::default()
+    };
+    let txout = TxOut {
+        value: Amount::from_sat(0x45),
+        script_pubkey: address.script_pubkey(),
+    };
+    let tx = Transaction {
+        input: vec![txin],
+        output: vec![txout],
+        version: Version::TWO,
+        lock_time: bitcoin::absolute::LockTime::Blocks(Height::ZERO),
+    };
+
+    let new_txid = rpc.send_raw_transaction(&tx).unwrap();
+    assert_ne!(txid, new_txid);
 }
