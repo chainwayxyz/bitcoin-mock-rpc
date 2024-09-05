b9381d69a7c403f27c52a3131fd00d129c9e1c35
Ceyhun Şen <ceyhuusen@gmail.com>
2024-09-05 15:37:27 +0300

    lib: Remove old RPC server.

--- src/lib.rs
+++ src/lib.rs
@@ -5,85 +5,36 @@
 //! way to check your Bitcoin operations without you needing to setup a Bitcoin
 //! environment.
 //!
-//! This library mocks [bitcoincore-rpc](https://github.com/rust-bitcoin/rust-bitcoincore-rpc)
+//! This library mocks
+//! [bitcoincore-rpc](https://github.com/rust-bitcoin/rust-bitcoincore-rpc)
 //! library. This mock takes advantage of `RpcApi` trait.
 //!
-//! This library is built upon
-//! [bitcoincore-rpc's](https://github.com/rust-bitcoin/rust-bitcoincore-rpc)
-//! `RpcApi` trait interface.
-//!
-//! ## Interfaces
+//! ## Interface
 //!
 //! ```text
-//!           ┌────────────────────┐
-//!           │                    │
-//!           │  User Application  │
-//!           │                    │
-//!           └──▲───────────────▲─┘
-//!              │               │
-//!              │        ┌──────┼───────┐
-//!              │        │              │
-//!              │        │  RPC Server  │
-//!              │        │   (Public)   │
-//!              │        └──────▲───────┘
-//!              │               │
-//!              │        ┌──────┼────────┐
-//! ┌────────────┴───┐    │               │
-//! │                │    │  RPC Adapter  │
-//! │  RpcApi Trait  │    │               │
-//! │   Interface    ┼───►│  encodes rust │
-//! │    (Public)    │    │  structs to   │
-//! └───────▲────────┘    │  hex strings  │
-//!         │             │               │
-//!  ┌──────┴──────┐      └───────────────┘
+//! ┌────────────────────┐
+//! │                    │
+//! │  User Application  │
+//! │                    │
+//! └────────▲───────────┘
+//!          │
+//!  ┌───────┴────────┐
+//!  │                │
+//!  │  RpcApi Trait  │
+//!  │   Interface    │
+//!  │                │
+//!  └───────▲────────┘
+//!          │
+//!  ┌───────┴─────┐
 //!  │             │
 //!  │ Mock Ledger │
 //!  │             │
 //!  └─────────────┴
 //! ```
 //!
-//! bitcoin-mock-rpc has 2 interfaces:
-//!
-//! 1. Rpc server: Similar experience as the Bitcoin RPC
-//! 2. `RpcApi` trait: No servers but a direct function call to ledger
-//!
-//! ### RPC Server
-//!
-//! RPC server can be spawned as long as there are available ports for them. Each
-//! server will have an independent blockchain.
-//!
-//! To run from CLI:
-//!
-//! ```bash
-//! $ cargo run
-//! Server started at 127.0.0.1:1024
-//! #                 ^^^^^^^^^^^^^^
-//! #         Use this address in applications
-//! $ cargo run -- --help # Prints usage information
-//! ```
-//!
-//! To run in a Rust application:
-//!
-//! ```rust
-//! fn test() {
-//!     // Calling `spawn_rpc_server` in a different test while this test is running
-//!     // is OK and will spawn another blockchain. If parameters are the same
-//!     // however, they will operate on the same blockchain. Note: (None, None)
-//!     // will result to pick random values.
-//!     let address = bitcoin_mock_rpc::spawn_rpc_server(None, None).unwrap();
-//!
-//!     let rpc =
-//!         bitcoincore_rpc::Client::new(&address.0.to_string(), bitcoincore_rpc::Auth::None).unwrap();
-//!
-//!     // Use `bitcoincore_rpc` as is from now on. No code change is needed.
-//! }
-//! ```
-//!
-//! ### `RpcApi`
-//!
 //! `RpcApiWrapper` trait can be used to select between real and mock RPC. It is
 //! a simple wrapper that allows you to also use methods like `Client::new()`.
-//! But it needs changes in your code:
+//! Needs changes in your code:
 //!
 //! ```rust
 //! use bitcoin_mock_rpc::RpcApiWrapper;
